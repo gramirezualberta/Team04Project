1e6ee9089063cf6ad2fc233a366ba847a66674c9
gramirezualberta <gramirez@ualberta.ca>
2014-02-25 11:22:46 -0700

    Add internet.java that check internet connection, also add "+" icon for action bar

--- src/com/example/team04project/GPSLocation.java
+++ src/com/example/team04project/GPSLocation.java
@@ -1,16 +1,13 @@
 package com.example.team04project;
 
-import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.util.Log;
 
 public class GPSLocation extends Service implements LocationListener {
