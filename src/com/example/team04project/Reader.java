7a6afb0247b8c64565914e370b3acf4a6d021a78
gramirezualberta <gramirez@ualberta.ca>
2014-02-24 20:06:01 -0700

    modified Reader.java

--- src/com/example/team04project/Reader.java
+++ src/com/example/team04project/Reader.java
@@ -1,10 +1,16 @@
 package com.example.team04project;
 
+import android.location.Location;
 
 
-public class Reader extends 
-User
+
+public class Reader extends User
 {
+	public Reader(Location userLocation) {
+		super(userLocation);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void browse(){
 		
 	}
