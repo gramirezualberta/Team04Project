087a9094c7f6ba806f3b212d15c353a19795d95c
gramirezualberta <gramirez@ualberta.ca>
2014-02-24 21:22:32 -0700

    Adding GPSLocation file that set the location & minor fix in the Auhtor.java and User.java files

--- src/com/example/team04project/Author.java
+++ src/com/example/team04project/Author.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 
 import android.location.Location;
 
+/**
+ * @author Guillermo Ramirez
+ * 
+ */
 public class Author extends User {
 
 	private Comments aNewComment;
+	// save all the top level comment create by the user
 	private ArrayList<TopLevel> topLevelList;
 
 	public Author(Location userLocation, String userName, Comments aNewComment) {
 		super(userLocation, userName);
 
 		this.aNewComment = aNewComment;
-		createTopLevel((TopLevel)aNewComment);
+		createTopLevel((TopLevel) aNewComment);
 	}
 
 	/**
@@ -33,10 +38,19 @@ public class Author extends User {
 		return topLevelList;
 	}
 
+	/**
+	 * After the user succefuly create a new TopLevel comment, this fuction will add the new top
+	 * level comment to the topLevelList.
+	 * @param aNewComment
+	 */
 	public void createTopLevel(TopLevel aNewComment) {
 		addTopLevelCommentToList(aNewComment);
 	}
 
+	/**
+	 * Add a new topLevel Comment to topLevelList
+	 * @param aNewTopLevel
+	 */
 	private void addTopLevelCommentToList(TopLevel aNewTopLevel) {
 		topLevelList.add(aNewTopLevel);
 	}
--- src/com/example/team04project/User.java
+++ src/com/example/team04project/User.java
@@ -6,22 +6,35 @@ import android.location.Location;
 
 /**
  * @author Guillermo Ramirez
- *
+ * 
  */
 public abstract class User {
-	protected Location userLocation;
+	protected Location userLocation;// actual location of the user
 	protected String userName = null;
-	protected final static String ID = generateID();
+	protected final static String ID = generateID();// ID to represent the user.
 
+	/**
+	 * Create a anonymous user with its location. 
+	 * @param userLocation
+	 */
 	public User(Location userLocation) {
 		this.userLocation = userLocation;
 	}
 
+	/**
+	 * create a new user with useraName
+	 * @param userLocation
+	 * @param userName
+	 */
 	public User(Location userLocation, String userName) {
 		this.userLocation = userLocation;
 		this.userName = userName;
 	}
 
+	/**
+	 *@return a ID for the user
+	 *
+	 */
 	private static String generateID() {
 		return UUID.randomUUID().toString();
 	}
