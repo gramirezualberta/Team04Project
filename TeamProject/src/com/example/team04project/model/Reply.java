095a76f43752436da90d0890883f3504528717f1
gramirezualberta <gramirez@ualberta.ca>
2014-03-04 12:32:46 -0700

    add parcelabe to Reply.java

--- TeamProject/src/com/example/team04project/model/Reply.java
+++ TeamProject/src/com/example/team04project/model/Reply.java
@@ -2,6 +2,9 @@ package com.example.team04project.model;
 
 import android.graphics.Bitmap;
 import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import android.util.Log;
 
 
 public class Reply extends Comments 
@@ -10,6 +13,14 @@ public class Reply extends Comments
 	public Reply(String textComment, Author aUser,
 			Bitmap aPicture) {
 		super(textComment, aUser, aPicture);
+	}
+	
+	private Reply(Parcel source) {
+		Log.e("Parcealbe",
+				"ParcelData(Parcel source): time to put back parcel data");
+		textComment = source.readString();
+		aUser = (Author) source.readValue(getClass().getClassLoader());
+		aPicture = (Bitmap) source.readValue(getClass().getClassLoader());
 	}
 
 	@Override
@@ -20,8 +31,24 @@ public class Reply extends Comments
 
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
-		// TODO Auto-generated method stub
+		Log.e("parceable", "" + flags);
+		dest.writeString(textComment);
+		dest.writeValue(aUser);
+		dest.writeValue(aPicture);      
 		
 	}
+	public static final Parcelable.Creator<Reply> CREATOR = new Creator<Reply>() {
+
+		@Override
+		public Reply[] newArray(int size) {
+			// TODO Auto-generated method stub
+			return new Reply [size];
+		}
+
+		@Override
+		public Reply createFromParcel(Parcel source) {
+			return new Reply(source);
+		}
+	};
 	
 }
